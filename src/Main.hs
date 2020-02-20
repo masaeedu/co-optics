8b035bab0bed93bf366a7c2390476087120441e4
Asad Saeeduddin <masaeedu@gmail.com>
2020-02-19 22:06:45 -0500

    Add example of composition

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.Profunctor (Profunctor(..))
 import Control.Arrow (Kleisli(..))
 import Control.Arrow (arr)
 import Data.Bifunctor.Joker (Joker(..))
@@ -9,19 +10,31 @@ import Profunctor.Re ()
 import Profunctor.Joker ()
 import Profunctor.Kleisli ()
 
-_int :: Prism' Double Int
-_int = prism b m
+real2int :: Prism' Double Int
+real2int = prism b m
   where
   b = fromIntegral
   m f =
     let r = round f
     in if fromIntegral r == f then Right r else Left f
 
+int2real :: Coprism' Int Double
+int2real = re real2int
+
+double :: Iso' Double Double
+double = dimap (/ 2) (* 2)
+
+halve :: Iso' Double Double
+halve = re double
+
 main :: IO ()
 main = do
   -- Since our arrow supports failure, we can work with integers as if they were doubles
-  print $ runKleisli @Maybe (re _int $ arr (/ 2)) $ 2 -- Just 1
-  print $ runKleisli @Maybe (re _int $ arr (/ 2)) $ 3 -- Nothing
+  print $ runKleisli @Maybe (int2real $ arr (/ 2)) $ 2 -- Just 1
+  print $ runKleisli @Maybe (int2real $ arr (/ 2)) $ 3 -- Nothing
 
   -- Another, slightly stupider notion of "arrow" is one with no input
-  print $ runJoker $ re _int $ Joker $ [42, 4.2, 5, 1.5]  -- [42, 5]
+  print $ runJoker $ int2real $ Joker [42, 4.2, 5, 1.5] -- [42, 5]
+
+  -- We can compose this stuff however much we like
+  print $ runJoker $ int2real . halve $ Joker $ [42, 4.2, 5, 1.5]  -- [21]
