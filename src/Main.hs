5e09f57eb693bd8678f15d8002ae957d07385c92
Asad Saeeduddin <masaeedu@gmail.com>
2020-02-19 20:44:05 -0500

    Add example with Joker

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,19 @@ import Data.Profunctor (Profunctor(..), Choice(..), Cochoice(..))
 import Control.Arrow (Kleisli(..))
 import Control.Applicative (Alternative(..))
 import Control.Arrow (arr)
+import Data.Bifunctor.Joker (Joker(..))
+
+class Functor f => Selective f
+  where
+  branch :: f (Either a b) -> (a -> c) -> (b -> c) -> f c
 
 instance (Monad m, Alternative m) => Cochoice (Kleisli m)
   where
   unleft (Kleisli amb) = Kleisli $ \a -> amb (Left a) >>= either pure (const empty)
+
+instance (Monad m, Alternative m) => Cochoice (Joker m)
+  where
+  unleft (Joker m) = Joker $ m >>= either pure (const empty)
 
 newtype Re p s t a b = Re { runRe :: p b a -> p t s }
 
@@ -24,10 +33,14 @@ instance Cochoice p => Choice (Re p s t)
   left' (Re p) = Re $ p . unleft
 
 type Optic p  s t a b = p a b -> p s t
-type Prism    s t a b = forall p. Choice p   => Optic p s t a b
-type Prism'   s   a   = Prism s s a a
-type Coprism  s t a b = forall p. Cochoice p => Optic p s t a b
-type Coprism' s   a   = Coprism s s a a
+
+type Iso      s t a b = forall p. Profunctor p => Optic p s t a b
+type Prism    s t a b = forall p. Choice     p => Optic p s t a b
+type Coprism  s t a b = forall p. Cochoice   p => Optic p s t a b
+
+type Iso'     s a = Iso     s s a a
+type Prism'   s a = Prism   s s a a
+type Coprism' s a = Coprism s s a a
 
 re :: Optic (Re p a b) s t a b -> Optic p b a t s
 re p = runRe $ p $ Re id
@@ -41,8 +54,8 @@ coprism cobuild comatch = unright . dimap (either id cobuild) comatch
 _Just :: Prism (Maybe a) (Maybe b) a b
 _Just = dimap (maybe (Right ()) Left) (either Just (const Nothing)) . left'
 
-_whole :: Prism' Double Int
-_whole = prism b m
+_int :: Prism' Double Int
+_int = prism b m
   where
   b = fromIntegral
   m f =
@@ -52,5 +65,8 @@ _whole = prism b m
 main :: IO ()
 main = do
   -- Since our arrow supports failure, we can work with integers as if they were doubles
-  print $ runKleisli @Maybe (re _whole $ arr (/ 2)) $ 2
-  print $ runKleisli @Maybe (re _whole $ arr (/ 2)) $ 3
+  print $ runKleisli @Maybe (re _int $ arr (/ 2)) $ 2 -- Just 1
+  print $ runKleisli @Maybe (re _int $ arr (/ 2)) $ 3 -- Nothing
+
+  -- Another, slightly stupider notion of "arrow" is one with no input
+  print $ runJoker $ re _int $ Joker $ [42, 4.2, 5, 1.5]  -- [42, 5]
