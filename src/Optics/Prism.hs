dec2abdcdd8c3f3d59c9504f9fd920f77b9de245
Asad Saeeduddin <masaeedu@gmail.com>
2020-03-16 20:22:48 -0400

    Prisms are Mux + Visitor

--- src/Optics/Prism.hs
+++ src/Optics/Prism.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric, ViewPatterns #-}
 module Optics.Prism (module Optics.Prism, module Optics.Types) where
 
 import MyPrelude
@@ -10,6 +10,8 @@ import Data.Bifunctor
 import Data.Profunctor
 import Data.List.NonEmpty (NonEmpty)
 
+import Control.Arrow ((&&&))
+
 import Data.Digit
 import Data.Generics.Wrapped (_Wrapped)
 
@@ -18,6 +20,7 @@ import Monoidal.Filterable
 
 import Profunctor.Joker
 import Profunctor.Kleisli
+import Profunctor.Mux
 
 import Optics.Types
 import Optics.Iso
@@ -34,15 +37,14 @@ mkt_id = Market id Right
 mkt_compose :: Market c d e f -> Market a b c d -> Market a b e f
 mkt_compose (Market b2 m2) (Market b1 m1) = Market (b2 . b1) (either Left (either (Left . b2) Right) . fmap m1 . m2)
 
-instance Profunctor (Market a b)
-  where
-  dimap f g (Market b m) = Market (g . b) (first g . m . f)
-
-instance Choice (Market a b) where
-  left' (Market b m) = Market (Left . b) (fwd assocE . second (fwd symmE) . fwd (re assocE) . first m)
-
 prism :: (b -> t) -> (s -> Either t a) -> Prism s t a b
 prism b m = dimap m (either id b) . right'
+
+toPrism :: Market a b s t -> Prism s t a b
+toPrism (Market b m) = prism b m
+
+fromPrism :: Prism s t a b -> Market a b s t
+fromPrism p = p mkt_id
 
 build :: Prism s t a b -> b -> t
 build p = _build $ p mkt_id
@@ -50,8 +52,30 @@ build p = _build $ p mkt_id
 match :: Prism s t a b -> s -> Either t a
 match p = _match $ p mkt_id
 
+instance Profunctor (Market a b)
+  where
+  dimap f g (Market b m) = Market (g . b) (first g . m . f)
+
+instance Choice (Market a b) where
+  left' (Market b m) = Market (Left . b) (fwd assocE . second (fwd symmE) . fwd (re assocE) . first m)
+
+instance Mux (Market a b)
+  where
+  Market b1 m1 /\ Market b2 m2 = Market (b1 &&& b2) (\(a, c) -> liftLeft (,) (m1 a) (m2 c))
+    where
+    liftLeft f (Left a)  (Left b)  = Left $ f a b
+    liftLeft _ (Right x) _         = Right x
+    liftLeft _ _         (Right x) = Right x
+
+instance Visitor (Market a b)
+  where
+  start = Market (const ()) (const $ Left ())
+
 liftPrism :: Decide f => Prism s t a b -> Prism (f s) (f t) (f a) (f b)
 liftPrism p = prism (fmap $ build p) (decide . fmap (match p))
+
+zipPrism :: Prism s t a b -> Prism s' t' a b -> Prism (s × s') (t × t') a b
+zipPrism (fromPrism -> m1) (fromPrism -> m2) = toPrism $ m1 /\ m2
 
 predicate :: (a -> Bool) -> Prism' a a
 predicate f = prism id (\x -> if f x then Right x else Left x)
