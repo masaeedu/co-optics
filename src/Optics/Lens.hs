b8e7f287af1b10478435faf8accd4143c2929686
Asad Saeeduddin <masaeedu@gmail.com>
2020-07-05 13:35:02 -0400

    Add more instances for lenses

--- src/Optics/Lens.hs
+++ src/Optics/Lens.hs
@@ -6,7 +6,11 @@ import Data.Profunctor
 import Data.Void
 
 import Monoidal.Applicative
+
+import Control.Arrow ((&&&), (***))
 import Profunctor.Demux
+import Profunctor.Mux
+
 import Optics.Types
 
 data Shop a b s t = Shop { _view :: s -> a, _update :: s -> b -> t }
@@ -53,6 +57,26 @@ liftLens l = lens (fmap $ view l) (liftA2 $ update l)
 
 altLens :: Lens s t a b -> Lens s' t' a b -> Lens (s + s') (t + t') a b
 altLens (fromLens -> s1) (fromLens -> s2) = toLens $ s1 \/ s2
+
+instance Semigroup a => Mux (Shop a b)
+  where
+  Shop v1 u1 /\ Shop v2 u2 = Shop (\(x, y) -> v1 x <> v2 y) (\(x, y) b -> (u1 x b, u2 y b))
+
+muxLens :: Semigroup a => Lens s t a b -> Lens s' t' a b -> Lens (s × s') (t × t') a b
+muxLens l1 l2 = toLens $ fromLens l1 /\ fromLens l2
+
+instance Monoid a => Visitor (Shop a b)
+  where
+  start = Shop (const mempty) (const $ const $ ())
+
+startLens :: Monoid a => Lens s () a b
+startLens = toLens start
+
+rmux :: Semigroup t => Shop a b s t -> Shop a' b' s t -> Shop (a × a') (b × b') s t
+rmux (Shop v1 u1) (Shop v2 u2) = Shop (v1 &&& v2) (\s -> uncurry (<>) . (u1 s *** u2 s))
+
+rstart :: Monoid t => Shop () b s t
+rstart = Shop (const ()) (const $ const $ mempty)
 
 emptyLens :: Lens Void x a b
 emptyLens = toLens stop
