c86fb601eb65370ceb38ff579132842b74cf7d8c
Asad Saeeduddin <masaeedu@gmail.com>
2020-08-15 21:16:45 -0400

    Remove monoidal functor stuff

--- src/Monoidal/BaseFunctor.hs
+++
@@ -1,8 +0,0 @@
-module Monoidal.BaseFunctor where
-
-import MyPrelude
-
-import qualified Control.Applicative as A
-
-newtype BaseFunctor f a = BaseFunctor { runBaseFunctor :: f a }
-  deriving (Functor, A.Applicative, A.Alternative)
--- src/Profunctor/Splice.hs
+++
@@ -1,9 +0,0 @@
-module Profunctor.Splice where
-
-import Data.Profunctor
-
-import MyPrelude
-
-class Profunctor p => Splice p
-  where
-  splice :: p a b -> p c d -> p (a + c) (b × d)
